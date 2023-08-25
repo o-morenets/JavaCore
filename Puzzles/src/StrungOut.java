1c839f53a85e37bc87249ae30d9e47f798954112
Oleksii Morenets <oleksii.morenets@empoweredsystems.com>
2023-08-26 00:09:41 +0200

    StrungOut (reuse name of class java.lang.String)

---
+++ Puzzles/src/StrungOut.java
@@ -0,0 +1,18 @@
+public class StrungOut {
+	public static void main(String[] args) { // Wrong signature of main method, because uses OUR own class String
+		String s = new String("Hello world");
+		System.out.println(s);
+	}
+}
+
+class String {
+	private final java.lang.String s;
+
+	public String(java.lang.String s) {
+		this.s = s;
+	}
+
+	public java.lang.String toString() {
+		return s;
+	}
+}
