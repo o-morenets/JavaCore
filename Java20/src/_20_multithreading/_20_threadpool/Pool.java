06f4426484e568cc45d79015c82424be59062552
Oleksii Morenets <oleksii.morenets@empoweredsystems.com>
2023-09-19 20:36:27 +0200

    Thread Pool

--- Java20/src/_20_multithreading/_20_threadpool/Pool.java
+++ Java20/src/_20_multithreading/_20_threadpool/Pool.java
@@ -2,25 +2,22 @@ package _20_multithreading._20_threadpool;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class Pool {
 
 	public static void main(String[] args) {
-//		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
-//		executor.submit(() -> {
-//			Thread.sleep(1000);
-//			return null;
-//		});
-//		executor.submit(() -> {
-//			Thread.sleep(1000);
-//			return null;
-//		});
-//		executor.submit(() -> {
-//			Thread.sleep(1000);
-//			return null;
-//		});
-
-		new Thread().start();
+		ExecutorService executor = Executors.newCachedThreadPool();
+		executor.submit(() -> {
+			Thread.sleep(1000);
+			return null;
+		});
+		executor.submit(() -> {
+			Thread.sleep(1000);
+			return null;
+		});
+		executor.submit(() -> {
+			Thread.sleep(1000);
+			return null;
+		});
 	}
 }
