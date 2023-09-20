217e95064e4ea3144e37dd952e1079437daa901c
Oleksii Morenets <oleksii.morenets@empoweredsystems.com>
2023-09-20 11:56:15 +0200

    Reordering

---
+++ Java20/src/_20_multithreading/_15_reordering/ReorderPrevention.java
@@ -0,0 +1,31 @@
+package _20_multithreading._15_reordering;
+
+public class ReorderPrevention {
+	private int a = 2;
+	private boolean flg = false;
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			ReorderPrevention reorderExample = new ReorderPrevention();
+			Thread thread1 = new Thread(() -> {
+				reorderExample.method1();
+			});
+			Thread thread2 = new Thread(() -> {
+				reorderExample.method2();
+			});
+			thread1.start();
+			thread2.start();
+		}
+	}
+
+	public synchronized void method1() {
+		a = 1;
+		flg = true;
+	}
+
+	public synchronized void method2() {
+		if (flg) {
+			System.out.println("a = " + a);
+		}
+	}
+}
