33735bb9931b17af00b6cdcb6dd90dae6090f996
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-03-27 12:28:43 +0100

    Fix

--- Java20/src/_08_oop/_00_class/_01_static_import/ImportStatic.java
+++ Java20/src/_08_oop/_00_class/_01_static_import/ImportStatic.java
@@ -1,17 +1,12 @@
 package _08_oop._00_class._01_static_import;
 
-import _08_oop._00_class.classexample.Employee;
-
 import static java.lang.System.out;
 
 public class ImportStatic {
 
 	public static void main(String[] args) {
 
-		/* Class Employee was imported from another package */
-		Employee harry = new Employee("Harry Hacker", 50000);
-
 		/* All System's static methods were imported above */
-		out.println("name=" + harry.getName() + ", salary=" + harry.getSalary());
+		out.println("Hello world");
 	}
 }
