359ffbc78bac90a319715b6c6cbc65a143a9eb05
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-04-16 14:32:42 +0200

    simulation - run into OOM

--- Java20/src/_15_exceptions/_99_out_of_memory_error/OutOfMemoryErrorDemo.java
+++ Java20/src/_15_exceptions/_99_out_of_memory_error/OutOfMemoryErrorDemo.java
@@ -2,15 +2,19 @@ package _15_exceptions._99_out_of_memory_error;
 
 public class OutOfMemoryErrorDemo {
 
-	private static final int SIZE = 1024 * 1024;
+    private static final int SIZE = 1024 * 1024;
 
-	public static void main(String[] args) {
-		Object[] tail = null;
-		int i = 0;
-		Runtime rt = Runtime.getRuntime();
-		while (true) {
-			System.out.printf("[%4s] Available heap memory: %s%n", i++, rt.freeMemory());
-			tail = new Object[]{tail, new Object[SIZE]};
-		}
-	}
+    public static void main(String[] args) {
+        runIntoOOM();
+    }
+
+    private static void runIntoOOM() {
+        Object[] tail = null;
+        int i = 0;
+        Runtime rt = Runtime.getRuntime();
+        while (true) {
+            System.out.printf("[%4d] Available heap memory: %s%n", i++, rt.freeMemory());
+            tail = new Object[]{tail, new Object[SIZE]};
+        }
+    }
 }
