8d5d90f4c42ff778a0a37682700ab56c7dc752ac
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-04-19 16:51:20 +0200

    BiPredicate Demo

--- Java20/src/_13_interfaces/_05_functional_interfaces/_99_samples/BiPredicateDemo_X.java
+++ Java20/src/_13_interfaces/_05_functional_interfaces/_99_samples/BiPredicateDemo_X.java
@@ -1,8 +1,10 @@
 package _13_interfaces._05_functional_interfaces._99_samples;
 
+import java.util.function.BiPredicate;
+
 public class BiPredicateDemo_X {
 
 	public static void main(String[] args) {
-		// BiPredicate<T, U> biPredicate;
+		 BiPredicate<String, Integer> biPredicate = (str, len) -> str.length() >= len;
 	}
 }
