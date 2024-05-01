685a9a77f2c33db333b30afaa8b1b17867f5c122
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-05-01 16:43:21 +0200

    classLoader

--- Java8_finalize_classLoader/src/_08_oop/_00_class/classLoader/PrintClassLoader.java
+++ Java8_finalize_classLoader/src/_08_oop/_00_class/classLoader/PrintClassLoader.java
@@ -1,5 +1,7 @@
 package _08_oop._00_class.classLoader;
 
+import com.sun.org.slf4j.internal.Logger;
+
 import java.util.ArrayList;
 
 public class PrintClassLoader {
@@ -12,8 +14,8 @@ public class PrintClassLoader {
 		System.out.println("Classloader of this class: "
 				+ PrintClassLoader.class.getClassLoader()); // jdk.internal.loader.ClassLoaders$AppClassLoader@7b3300e5
 
-		System.out.println("Classloader of com.sun.javafx.binding.Logging: "
-				+ System.Logger.class.getClassLoader()); // null
+		System.out.println("Classloader of com.sun.org.slf4j.internal.Logger: "
+				+ Logger.class.getClassLoader()); // null
 
 		System.out.println("Classloader of ArrayList: "
 				+ ArrayList.class.getClassLoader()); // null, because Bootstrap CL implemented in C++
