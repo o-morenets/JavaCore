6758f220bf87a092a55e5306b627e7ddf550f811
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-05-03 21:18:24 +0200

    CloneExample // min fixes

--- Core/src/_13_interfaces/_02_cloneable/CloneExample.java
+++ Core/src/_13_interfaces/_02_cloneable/CloneExample.java
@@ -3,13 +3,14 @@ package _13_interfaces._02_cloneable;
 public class CloneExample {
 
 	@Override
-	public Object clone() throws CloneNotSupportedException {
+	public Object clone() {
 		return new CloneExample();
 	}
 
 	public static void main(String[] args) throws CloneNotSupportedException {
 		CloneExample cloneExample = new CloneExample();
 		CloneExample clone = (CloneExample) cloneExample.clone();
-		System.out.println(clone);
+		System.out.println("clone origin = " + cloneExample);
+		System.out.println("clone		 = " + clone);
 	}
 }
