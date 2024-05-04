644dcf387d765c1484709e6585de3b22fd1987c0
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-05-04 13:41:52 +0200

    ExecutorServiceExamples

--- Core/src/_20_multithreading/_20_executorservice/ExecutorServiceExamples.java
+++ Core/src/_20_multithreading/_20_executorservice/ExecutorServiceExamples.java
@@ -3,47 +3,52 @@ package _20_multithreading._20_executorservice;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ExecutorServiceExamples {
 
-	private static Runnable initRunnable() {
-		Runnable runnableTask = () -> {
+	private static Runnable initRunnableTask() {
+        return () -> {
 			System.out.println("Runnable starts...");
-			try {
-				TimeUnit.MILLISECONDS.sleep(300);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			System.out.println("Runnable finished.");
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Runnable finished.");
 		};
-		return runnableTask;
 	}
 
-	private static Callable<String> initCallable(int num) {
-		Callable<String> callableTask = () -> {
-			TimeUnit.MILLISECONDS.sleep(300);
-			return "Task #" + num + " execution";
+	private static Callable<String> initCallableTask(int num) {
+        return () -> {
+			System.out.println("Callable task #" + num + " is running...");
+			TimeUnit.SECONDS.sleep(2);
+			return "Task #" + num + " result";
 		};
-		return callableTask;
 	}
 
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-		Runnable runnableTask = initRunnable();
+		Runnable runnableTask = initRunnableTask();
 
-		Callable<String> callableTask = initCallable(10);
+		Callable<String> callableTask = initCallableTask(0);
 
-		List<Callable<String>> callableTasks = new ArrayList<>();
-		callableTasks.add(initCallable(1));
-		callableTasks.add(initCallable(2));
-		callableTasks.add(initCallable(3));
+		List<Callable<String>> callableTasks = IntStream.rangeClosed(1, 10).mapToObj(ExecutorServiceExamples::initCallableTask).collect(Collectors.toList());
 
-		// create a thread pool with 10 threads:
+		// create a thread pool:
 		ExecutorService executorService =
-				new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS,
-						new LinkedBlockingQueue<Runnable>());
+				new ThreadPoolExecutor(2, 5, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+
 		// or
-//		ExecutorService executor = Executors.newFixedThreadPool(10);
+//		executorService = Executors.newSingleThreadExecutor();
+//		executorService = Executors.newFixedThreadPool(10);
+//		executorService = Executors.newCachedThreadPool();
+//		executorService = Executors.newScheduledThreadPool(5);
+//		executorService = Executors.newSingleThreadScheduledExecutor();
+//		executorService = Executors.newThreadPerTaskExecutor(Thread::new);
+//		executorService = Executors.newVirtualThreadPerTaskExecutor();
+//		executorService = Executors.newWorkStealingPool();
 
 		// The execute() method is void and doesn't give any possibility to get the result of a task’s execution or to check the task’s status (is it running):
 		executorService.execute(runnableTask);
@@ -61,8 +66,24 @@ public class ExecutorServiceExamples {
 		// of all task executions in the form of a list of objects of type Future:
 		List<Future<String>> futures = executorService.invokeAll(callableTasks);
 		System.out.println("All results: " + futures);
+		printFuturesResults(futures);
+
+		boolean termination = executorService.awaitTermination(5, TimeUnit.SECONDS);
+		System.out.println(termination);
 
 		List<Runnable> notExecutedTasks = executorService.shutdownNow();
 		System.out.println("notExecutedTasks = " + notExecutedTasks);
+	}
+
+	private static void printFuturesResults(List<Future<String>> futures) {
+		futures.forEach(f -> {
+            try {
+                System.out.println(f.get());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
 	}
 }
