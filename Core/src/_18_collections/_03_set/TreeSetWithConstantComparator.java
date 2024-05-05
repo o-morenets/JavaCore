bfe162431385ae56de613251cb38cf06aa02b13d
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-05-05 12:04:07 +0200

    TreeSet With Constant Comparator

---
+++ Core/src/_18_collections/_03_set/TreeSetWithConstantComparator.java
@@ -0,0 +1,18 @@
+package _18_collections._03_set;
+
+import java.util.TreeSet;
+import java.util.stream.IntStream;
+
+/**
+ * TreeSet uses Comparator which always returns 0
+ * As a result, different objects (by equals() method which is not used here) are considered as equal and can be added only once
+ */
+public class TreeSetWithConstantComparator {
+
+    public static void main(String[] args) {
+
+        IntStream.of(23, 23, 17)
+                .collect(() -> new TreeSet<>((o1, o2) -> 0), TreeSet::add, TreeSet::addAll)
+                .forEach(System.out::println); // 23 (only one element)
+    }
+}
