487378d2b8de8a9b8b7654c645d8568a85143502
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-05-05 14:06:25 +0200

    EnumMap

---
+++ Core/src/_18_collections/_04_map/EnumMap.java
@@ -0,0 +1,17 @@
+package _18_collections._04_map;
+
+public class EnumMap {
+
+    enum WeekDays {
+        MONDAY,
+        TUESDAY
+    }
+
+    public static void main(String[] args) {
+        java.util.EnumMap<WeekDays, Integer> salesMap = new java.util.EnumMap<>(WeekDays.class);
+        salesMap.put(WeekDays.MONDAY, 1000);
+        salesMap.put(WeekDays.TUESDAY, 2500);
+
+        System.out.println("salesMap = " + salesMap);
+    }
+}
