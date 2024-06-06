b071dcaf6b01d8cabbfb8ae51264bf2ea0dfa071
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-06-06 12:15:01 +0200

    hackerrank - Candles

---
+++ Exercises/src/hackerrank/Candles.java
@@ -0,0 +1,15 @@
+package hackerrank;
+
+import java.util.List;
+
+public class Candles {
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        // Write your code here
+        return candles.stream()
+//                .mapToInt(Integer::valueOf)
+//                .max()
+                .reduce(Integer::sum)
+                .map(i -> candles.stream().mapToInt(Integer::valueOf).filter(c -> c == i).count()).orElse(-1L).intValue();
+    }
+}
