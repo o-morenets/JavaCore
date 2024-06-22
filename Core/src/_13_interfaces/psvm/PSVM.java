a43e60ab9a80b8b13045fd98ccf520590678fbb6
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-06-22 23:38:56 +0200

    psvm in interface

--- Core/src/_13_interfaces/psvm/PSVM.java
+++ Core/src/_13_interfaces/psvm/PSVM.java
@@ -1,8 +1,12 @@
 package _13_interfaces.psvm;
 
+/**
+ * We can run it (since java 11)
+ * java _13_interfaces.psvm.PSVM.java arg1 arg2 "arg 3" etc...
+ */
 public interface PSVM {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         for (String arg : args) {
             System.out.println(arg);
         }
--- Core/src/_13_interfaces/psvm/PSVMTest.java
+++ Core/src/_13_interfaces/psvm/PSVMTest.java
@@ -1,6 +1,7 @@
 package _13_interfaces.psvm;
 
 public class PSVMTest implements PSVM {
+
 	public static void main(String[] args) {
 		System.out.println("Class");
 		PSVM.main(new String[]{"Interface"});
