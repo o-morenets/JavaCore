6a31ec3bece0f3cc84ed544867621ccfa1bae9ee
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-06-23 12:04:42 +0200

    Multithreading - race condition

---
+++ Core/src/_20_multithreading/_12_volatile/race_condition/RaceConditionDemo.java
@@ -0,0 +1,48 @@
+package _20_multithreading._12_volatile.race_condition;
+
+class Counter implements Runnable {
+
+    private int c = 0;
+
+    public void increment() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        c++;
+    }
+
+    public void decrement() {
+        c--;
+    }
+
+    public int getValue() {
+        return c;
+    }
+
+    @Override
+    public /*synchronized */ void run() {
+
+        // incrementing
+        this.increment();
+        System.out.println("Value for Thread After increment " + Thread.currentThread().getName() + " " + this.getValue());
+
+        // decrementing
+        this.decrement();
+        System.out.println("Value for Thread at last " + Thread.currentThread().getName() + " " + this.getValue());
+    }
+}
+
+public class RaceConditionDemo {
+
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        Thread t1 = new Thread(counter, "Thread-1");
+        Thread t2 = new Thread(counter, "Thread-2");
+        Thread t3 = new Thread(counter, "Thread-3");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
