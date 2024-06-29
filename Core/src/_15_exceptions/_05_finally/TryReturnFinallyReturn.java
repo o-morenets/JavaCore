1da4f66aa2865b28a76fe0a3907ef61446b6261c
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-06-29 20:29:42 +0200

    try without catch

--- Core/src/_15_exceptions/_05_finally/TryReturnFinallyReturn.java
+++ Core/src/_15_exceptions/_05_finally/TryReturnFinallyReturn.java
@@ -8,14 +8,21 @@ public class TryReturnFinallyReturn {
 		System.err.println(f());
 	}
 
+	// this method must return a value either from try or finally OR System.exit() OR throw an Exception
 	public static int f() {
 		try {
-			return 0;
+			System.out.println("try...");
+//			return 0;
 			// or
-//            throw new RuntimeException();
+//			throw new RuntimeException();
 		} finally {
+			System.out.println("finally...");
 			return 1;
+			// or
+//			throw new RuntimeException();
 		}
+		// or
+//		throw new RuntimeException();
 	}
 
 	// >> 1
