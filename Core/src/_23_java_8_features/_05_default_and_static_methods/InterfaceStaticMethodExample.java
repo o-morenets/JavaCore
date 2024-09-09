e29bd1f5ff96891c1feef63eb5627b50b10615f9
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-09-09 20:42:17 +0200

    InterfaceStaticMethodExample

---
+++ Core/src/_23_java_8_features/_05_default_and_static_methods/InterfaceStaticMethodExample.java
@@ -0,0 +1,31 @@
+package _23_java_8_features._05_default_and_static_methods;
+
+
+interface MyData {
+
+    default void print(String str) {
+        if (!isNull(str))
+            System.out.println("MyData Print::" + str);
+    }
+
+    static boolean isNull(String str) {
+        System.out.println("Interface Null Check");
+
+        return str == null ? true : "".equals(str) ? true : false;
+    }
+}
+
+public class InterfaceStaticMethodExample implements MyData {
+
+    public boolean isNull(String str) {
+        System.out.println("Impl Null Check");
+
+        return str == null ? true : false;
+    }
+
+    public static void main(String args[]) {
+        InterfaceStaticMethodExample obj = new InterfaceStaticMethodExample();
+        obj.print("");
+        obj.isNull("abc");
+    }
+}
