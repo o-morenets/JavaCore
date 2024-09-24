964f63b8d2618d0804cec78eb57b2f1ef79f1fcd
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-09-24 20:09:51 +0200

    HashMapVsLinkedHashMap // fix keys

--- Core/src/_18_collections/_04_map/HashMapVsLinkedHashMap.java
+++ Core/src/_18_collections/_04_map/HashMapVsLinkedHashMap.java
@@ -9,16 +9,16 @@ public class HashMapVsLinkedHashMap {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
         fillMap(map);
-        System.out.println("map = " + map); // map = {1=B, 2=C, 3=D, 4=E, 5=F} - because hashCode of Integer is the number itself
+        System.out.println("map = " + map); // map = {1=A, 2=B, 3=C, 4=D, 5=E} - because hashCode of Integer is the number itself
 
         map = new LinkedHashMap<>();
         fillMap(map);
-        System.out.println("map = " + map); // map = {5=F, 4=E, 3=D, 2=C, 1=B} - in the same order as we inserted
+        System.out.println("map = " + map); // map = {5=E, 4=D, 3=C, 2=B, 1=A} - in the same order as we inserted
     }
 
     private static void fillMap(Map<Integer, String> map) {
         for (int i = 5; i >= 1; i--) {
-            map.put(i, String.valueOf(Character.valueOf((char) ('A' + i))));
+            map.put(i, String.valueOf(Character.valueOf((char) ('A' + i - 1))));
         }
     }
 }
