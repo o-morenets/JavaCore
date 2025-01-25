c34b3a85561981b0df88024475debc6c3ea493af
Oleksii Morenets <oleksii.morenets@gmail.com>
2025-01-25 19:38:45 +0100

    LeetCode 2785 solution // added description

--- Exercises/src/leetcode/_2785_Sort_Vowels_in_a_String/Solution.java
+++ Exercises/src/leetcode/_2785_Sort_Vowels_in_a_String/Solution.java
@@ -2,6 +2,40 @@ package leetcode._2785_Sort_Vowels_in_a_String;
 
 import java.util.*;
 
+/**
+ * 2785. Sort Vowels in a String
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Hint
+ * Given a 0-indexed string s, permute s to get a new string t such that:
+ *
+ * All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+ * The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+ * Return the resulting string.
+ *
+ * The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "lEetcOde"
+ * Output: "lEOtcede"
+ * Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+ * Example 2:
+ *
+ * Input: s = "lYmpH"
+ * Output: "lYmpH"
+ * Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 105
+ * s consists only of letters of the English alphabet in uppercase and lowercase.
+ */
 class Solution {
     public String sortVowels(String s) {
         final Set<Character> vowels = Set.of('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
@@ -24,7 +58,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.sortVowels("lEetcOde"));
-        System.out.println(solution.sortVowels("lYmpH"));
+        System.out.println(solution.sortVowels("lEetcOde")); // "lEOtcede"
+        System.out.println(solution.sortVowels("lYmpH")); // "lYmpH"
     }
 }
