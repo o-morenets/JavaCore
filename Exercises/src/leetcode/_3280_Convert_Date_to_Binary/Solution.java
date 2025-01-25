1a6cd004361fe902c2c5f2c3461060510abc8668
Oleksii Morenets <oleksii.morenets@gmail.com>
2025-01-25 19:36:52 +0100

    LeetCode 3280 solution // added description

--- Exercises/src/leetcode/_3280_Convert_Date_to_Binary/Solution.java
+++ Exercises/src/leetcode/_3280_Convert_Date_to_Binary/Solution.java
@@ -1,5 +1,47 @@
 package leetcode._3280_Convert_Date_to_Binary;
 
+/**
+ * 3280. Convert Date to Binary
+ * Solved
+ * Easy
+ * Topics
+ * Companies
+ * You are given a string date representing a Gregorian calendar date in the yyyy-mm-dd format.
+ *
+ * date can be written in its binary representation obtained by converting year, month, and day to their binary representations without any leading zeroes and writing them down in year-month-day format.
+ *
+ * Return the binary representation of date.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: date = "2080-02-29"
+ *
+ * Output: "100000100000-10-11101"
+ *
+ * Explanation:
+ *
+ * 100000100000, 10, and 11101 are the binary representations of 2080, 02, and 29 respectively.
+ *
+ * Example 2:
+ *
+ * Input: date = "1900-01-01"
+ *
+ * Output: "11101101100-1-1"
+ *
+ * Explanation:
+ *
+ * 11101101100, 1, and 1 are the binary representations of 1900, 1, and 1 respectively.
+ *
+ *
+ *
+ * Constraints:
+ *
+ * date.length == 10
+ * date[4] == date[7] == '-', and all other date[i]'s are digits.
+ * The input is generated such that date represents a valid Gregorian calendar date between Jan 1st, 1900 and Dec 31st, 2100 (both inclusive).
+ */
 class Solution {
     public String convertDateToBinary(String date) {
         String[] dateParts = date.split("-");
@@ -15,6 +57,8 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().convertDateToBinary("2020-1-1"));
+        Solution solution = new Solution();
+        System.out.println(solution.convertDateToBinary("2080-02-29")); // 100000100000-10-11101
+        System.out.println(solution.convertDateToBinary("1900-01-01")); // 11101101100-1-1
     }
 }
