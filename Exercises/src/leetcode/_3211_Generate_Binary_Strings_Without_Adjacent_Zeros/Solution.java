c148b481960cfeaffca71d19f47696f3a13fb6fa
Oleksii Morenets <omorenets@atlassian.com>
2025-01-25 19:40:37 +0100

    LeetCode 3211 solution // added description

--- Exercises/src/leetcode/_3211_Generate_Binary_Strings_Without_Adjacent_Zeros/Solution.java
+++ Exercises/src/leetcode/_3211_Generate_Binary_Strings_Without_Adjacent_Zeros/Solution.java
@@ -3,6 +3,49 @@ package leetcode._3211_Generate_Binary_Strings_Without_Adjacent_Zeros;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 3211. Generate Binary Strings Without Adjacent Zeros
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Hint
+ * You are given a positive integer n.
+ *
+ * A binary string x is valid if all
+ * substrings
+ *  of x of length 2 contain at least one "1".
+ *
+ * Return all valid strings with length n, in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 3
+ *
+ * Output: ["010","011","101","110","111"]
+ *
+ * Explanation:
+ *
+ * The valid strings of length 3 are: "010", "011", "101", "110", and "111".
+ *
+ * Example 2:
+ *
+ * Input: n = 1
+ *
+ * Output: ["0","1"]
+ *
+ * Explanation:
+ *
+ * The valid strings of length 1 are: "0" and "1".
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 18
+ */
 class Solution {
     public List<String> validStrings(int n) {
         List<String> result = new ArrayList<>();
