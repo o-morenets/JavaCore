fc09d76dd4c955f9e117d136ead4b59dccc9f4cb
Oleksii Morenets <omorenets@atlassian.com>
2025-03-19 23:42:32 +0100

    LeetCode 451 solution

---
+++ Exercises/src/leetcode/_451_Sort_Characters_By_Frequency/Solution.java
@@ -0,0 +1,60 @@
+package leetcode._451_Sort_Characters_By_Frequency;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * 451. Sort Characters By Frequency
+ * Medium
+ * Topics
+ * Companies
+ * Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+ * <p>
+ * Return the sorted string. If there are multiple answers, return any of them.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "tree"
+ * Output: "eert"
+ * Explanation: 'e' appears twice while 'r' and 't' both appear once.
+ * So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+ * Example 2:
+ * <p>
+ * Input: s = "cccaaa"
+ * Output: "aaaccc"
+ * Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+ * Note that "cacaca" is incorrect, as the same characters must be together.
+ * Example 3:
+ * <p>
+ * Input: s = "Aabb"
+ * Output: "bbAa"
+ * Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+ * Note that 'A' and 'a' are treated as two different characters.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 5 * 105
+ * s consists of uppercase and lowercase English letters and digits.
+ */
+public class Solution {
+    public String frequencySort(String s) {
+        return s.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .map(e -> String.valueOf(e.getKey()).repeat(e.getValue().intValue()))
+                .collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.frequencySort("tree")); // eert
+        System.out.println(solution.frequencySort("cccaaa")); // aaaccc
+    }
+}
