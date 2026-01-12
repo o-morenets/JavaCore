0509fc306595c0393da30788a20de08a95b8fe40
Oleksii Morenets <oleksii.morenets@gmail.com>
2026-01-12 12:56:00 +0100

    Interview Task Luxoft 12.01.2026

---
+++ Exercises/src/misc/SumOfDistinctNumbers.java
@@ -0,0 +1,47 @@
+package misc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 12.01.2026
+ * Luxoft interview
+ *
+ * Input : A[] = {1, 5, 3, 8, 1}
+ *         B[] = {5, 4, 6, 7}
+ * Output : 28
+ * 3 + 4 + 7 + 6 + 8 = 28
+ *
+ * Input : A[] = {1, 5, 3, 8}
+ *         B[] = {5, 1, 8, 3}
+ * Output : 0
+ */
+public class SumOfDistinctNumbers {
+
+    static int sumOfDistinctNumbers(int[] a, int[] b) {
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : a) {
+            Integer count = freq.get(num);
+            if (count == null) count = 0;
+            freq.put(num, count + 1);
+        }
+
+        for (int num : b) {
+            Integer count = freq.get(num);
+            if (count == null) count = 0;
+            freq.put(num, count + 1);
+        }
+
+        return freq.entrySet().stream()
+                .filter(e -> e.getValue() == 1)
+                .map(e -> e.getKey())
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    static void main() {
+        System.out.println(sumOfDistinctNumbers(new int[]{1, 5, 3, 8, 1}, new int[]{5, 4, 6, 7})); // 28
+        System.out.println(sumOfDistinctNumbers(new int[]{1, 5, 3, 8}, new int[]{5, 1, 8, 3})); // 0
+    }
+}
