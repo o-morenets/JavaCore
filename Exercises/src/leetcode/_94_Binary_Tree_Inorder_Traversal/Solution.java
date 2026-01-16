d545c00ae3ced5544d4e476ad968945326da616a
Oleksii Morenets <oleksii.morenets@gmail.com>
2026-01-16 23:06:06 +0100

    LeetCode 94 solution

---
+++ Exercises/src/leetcode/_94_Binary_Tree_Inorder_Traversal/Solution.java
@@ -0,0 +1,83 @@
+package leetcode._94_Binary_Tree_Inorder_Traversal;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 94. Binary Tree Inorder Traversal
+ *
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * Example 1:
+ *
+ * Input: root = [1,null,2,3]
+ *
+ * Output: [1,3,2]
+ *
+ * Example 2:
+ *
+ * Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+ *
+ * Output: [4,2,6,5,7,1,3,9,8]
+ *
+ * Example 3:
+ *
+ * Input: root = []
+ *
+ * Output: []
+ *
+ * Example 4:
+ *
+ * Input: root = [1]
+ *
+ * Output: [1]
+ *
+ * Recursive solution is trivial, could you do it iteratively?
+ */
+public class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        var current = root;
+
+        while (!stack.isEmpty() || current != null) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            res.add(current.val);
+
+            current = current.right;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        // TODO
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
